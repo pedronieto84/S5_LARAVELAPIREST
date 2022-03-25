9581d2e400c88c2f93de2c8ecfaf6a1d99a0a219
AlVirtual <virtualweb2020@gmail.com>
2022-03-25 12:09:50 +0100

    comprovadas las rutas con postman, parece que funciona

--- routes/api.php
+++ routes/api.php
@@ -21,27 +21,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('players', [PlayerController::class, 'store'])->name('players.store');
-Route::put('players/{player}', [PlayerController::class, 'update'])->name('players.update');
-Route::post('players/{player}/games', [ShotController::class, 'store'])->name('players.store');
-Route::delete('players/{player}/games', [ShotController::class, 'destroy'])->name('players.destroy');
+Route::post('players', [PlayerController::class, 'store'])->name('players.store');      // crea un jugador
+Route::put('players/{player}', [PlayerController::class, 'update'])->name('players.update');   // modifica el nom del jugador
+Route::post('players/{player}/games', [ShotController::class, 'store'])->name('players.store');  // un jugador específic realitza una tirada dels daus.
+Route::delete('players/{player}/games', [ShotController::class, 'destroy'])->name('players.destroy');  // elimina les tirades del jugador
 
-Route::get('players', [PlayerController::class, 'index'])->name('players.index');//revisar
+Route::get('players', [PlayerController::class, 'index'])->name('players.index');//revisar  // retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits 
 
-Route::get('players/{player}/games', [ShotController::class, 'show'])->name('players.index');
-Route::get('players/ranking', [PlayerController::class, 'rank'])->name('players.rank');
-Route::get('players/ranking/loser', [PlayerController::class, 'loser'])->name('players.rankloser');
-Route::get('players/ranking/winner', [PlayerController::class, 'winner'])->name('players.rankwinner');
-
-
+Route::get('players/{player}/games', [ShotController::class, 'show'])->name('players.index');  // retorna el llistat de jugades per un jugador.
+Route::get('players/ranking', [PlayerController::class, 'rank'])->name('players.rank');  // retorna el ranking mig de tots els jugadors del sistema. És a dir, el percentatge mig d’èxits.
+Route::get('players/ranking/loser', [PlayerController::class, 'loser'])->name('players.rankloser');  // retorna el jugador amb pitjor percentatge d’èxit
+Route::get('players/ranking/winner', [PlayerController::class, 'winner'])->name('players.rankwinner'); // retorna el jugador amb pitjor percentatge d’èxit.
 
 
-// POST /players                  OK       // crea un jugador
-// PUT /players/{id}              OK       // modifica el nom del jugador
-// POST /players/{id}/games/      OK       // un jugador específic realitza una tirada dels daus.
-// DELETE /players/{id}/games     OK       // elimina les tirades del jugador
-// GET /players                   casi OK        // retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits 
-// GET /players/{id}/games        OK       // retorna el llistat de jugades per un jugador.
-// GET /players/ranking           OK         // retorna el ranking mig de tots els jugadors del sistema. És a dir, el percentatge mig d’èxits.
-// GET /players/ranking/loser     OK         // retorna el jugador amb pitjor percentatge d’èxit
-// GET /players/ranking/winner    OK         // retorna el jugador amb pitjor percentatge d’èxit.
+
